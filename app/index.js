a2a642a8eaa3c6163a951521b6c2da1ae4d8a7ff
skyzluo <skyzluo@users.noreply.github.com>
2017-01-31 16:31:57 -0500

    Update index.js

--- app/index.js
+++ app/index.js
@@ -1,10 +1,12 @@
-import React from 'react';
 var React = require('react');
 var ReactDOM = require('react-dom');
 
-function Hello() {
-  // Correct! This use of <div> is legitimate because div is a valid HTML tag:
-  return <div>Hello</div>;
-}
+var Hello = React.createClass({
+  render: function () {
+    return (
+      <div>Hello ReactJS Program!</div>
+    )
+  }
+});
 
 ReactDOM.render(<Hello />, document.getElementById('app'));
